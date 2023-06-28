209d515eefdd4f21ca19f0cf80384d61af332faf
laon2019 <laon5987@nate.com>
2023-06-28 22:52:21 +0900

    phone-book

---
+++ codingnoona/phonebook/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import ContactForm from './component/ContactForm';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ContactList from './component/ContactList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col>
+            <ContactList />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
